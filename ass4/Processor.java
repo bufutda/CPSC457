3b8190e192cabd15695e4c96f7f7e57b101cd8d9
Bufutda <saw@tzky.ca>
2016-11-21 14:04:02 -0700

    Assignemnt4/SP

--- ass4/Processor.java
+++ ass4/Processor.java
@@ -20,9 +20,40 @@ public class Processor {
     private MemoryAgent memAgent;
 
     /**
-     * Implementation of Peterson's n-process algorithm (in this case n = 10)
+     * Fields to permit Peterson's algorithm
+     * flag: -1, 0, ..., 8
+     * turn: 0, 1, ..., 9
      */
-    public void petersonCritical () {
+    private static int[10] flag;
+    private static int[10] turn;
+    private static int procCount;
 
+    /**
+     * The processor number
+     */
+    private int pNo;
+
+    public Processor (int pNo) {
+
+    }
+
+    public void EntrySection () {
+        for (int k = 0; k <= Processor.procCount - 2) {
+            Processor.flag[pNo] = k;
+            Processor.turn[k] = pNo;
+            while (E(j != i, Processor.flag[j] >= k && Processor.turn[k] == i)) {
+                nop();
+            }
+        }
+    }
+
+    public void ExitSection () {
+        Processor.flag[pNo] = -1;
+    }
+
+    public int main () {
+        // Peterson's n-process algorithm for n = 10
+        flag.fill(-1);
+        turn.fill(0);
     }
 }
